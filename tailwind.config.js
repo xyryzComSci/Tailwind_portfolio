4d2cb7294c80399bd820ee9f7f08dd4f00568479
Xyryz <xyryzpatagnankamias@gmail.com>
2024-10-04 21:39:56 +0800

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*/*.html"],
+  theme: {
+    screens: {
+      'sm': '765px',
+    },
+  fontFamily:{
+    fredoka:['Fredoka', 'sans-serif']
+  }
+  },
+  plugins: [],
+}
+
